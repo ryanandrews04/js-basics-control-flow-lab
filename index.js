b5faa20059fd6e2a96defb4bc9b8f03b07c91eb9
Ryan Andrews <rcandrews44@gmail.com>
2021-04-05 16:38:29 -0400

    commit tree 29c2b9db1a26ffb3e64a5a4c8f2c3ef26b844de3

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  if (feet <= 400)
+    return `This one is on me!`;
+  else if (feet > 2000 && feet <= 2500)
+    return 'I will gladly take your thirty bucks.';
+  else if (feet > 2500)
+    return 'No can do.'
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(NYC){
+  return (NYC ? "Ok, sounds good." : "No can do.");
+
+  
 }
 
 function switchOnCharmFromTip(){
